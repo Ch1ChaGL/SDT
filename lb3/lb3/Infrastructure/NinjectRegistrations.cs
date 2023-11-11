3feadfcf774dcc5e83ddeb3ec92311c07a9382dd
Danil Markov <markov.danil.04@mail.ru>
2023-11-12 00:09:38 +0300

    lb3

---
+++ lb3/lb3/Infrastructure/NinjectRegistrations.cs
@@ -0,0 +1,31 @@
+﻿using lb3.Models;
+using Ninject.Modules;
+using Ninject.Web.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lb3.Infrastructure
+{
+    public class NinjectRegistrations : NinjectModule
+    {
+        public override void Load()
+        {
+            //В данном случае создается только один экземпляр
+            Bind<IValueCalculator>().To<LinqValueCalculator>().InRequestScope();
+
+            //В этом случае создается два экземпляра на каждый параметр
+            //Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            //Привязка к свойству
+            //Bind<IDiscountHelper>().To<DefaulDiscountHelper>().WithPropertyValue("DiscountSize", 50m);
+
+            //Привязка к значению конструктора
+            Bind<IDiscountHelper>().To<DefaulDiscountHelper>().WithConstructorArgument("discountParam", 10m);
+
+            //Указание использовать класс FlexibleDiscountHelper как реализациию IDiscountHelper, при вызове LinqValueCalculator, таким образом отработает сначала первая привязка, а после нее эта
+            Bind<IDiscountHelper>().To<FlexibleDiscountHelper>().WhenInjectedInto<LinqValueCalculator>();
+        }
+    }
+}
---
+++ lb3/lb3/Models/Discount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lb3.Models
+{
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+
+    public class DefaulDiscountHelper : IDiscountHelper
+    {
+        
+        public DefaulDiscountHelper(decimal discountParam)
+        {
+            DiscountSize = discountParam;
+        }
+        public decimal DiscountSize { get; set; }
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (DiscountSize / 100m * totalParam));
+        }
+
+    }
+}
